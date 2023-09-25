656a46c1d87c22453dec1c0da395bd40dc6202f7
nalma <nalma@Nabil>
2023-09-25 08:58:47 +0300

    Added a print statement

--- src/testproject/TestProject.java
+++ src/testproject/TestProject.java
@@ -3,7 +3,7 @@ package testproject;
 public class TestProject {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");     
     }
     
 }
