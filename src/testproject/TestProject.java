5e0524e86276bc43ccd775b71ec070c9ac554153
nalmashfi <115117963+nalmashfi@users.noreply.github.com>
2023-09-26 11:20:33 +0300

    Added a comment to the Add method

--- src/testproject/TestProject.java
+++ src/testproject/TestProject.java
@@ -5,7 +5,8 @@ public class TestProject {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    // add method
     public static int add(int x, int y) {
         return x+y;
     }
